8b239dbb14aba848c8d5d0e5745801a02b76c00c
SHANKU ACHYUTH <71203243+shankuachyuth@users.noreply.github.com>
2025-04-26 16:27:13 +0000

    dataypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId =144553
+let accountEmail ="js123@gmail.com"
+var accountPassword = "12345"
+accountCity ="Hyderabad"
+
+//accountId = 2 // Not allowed
+accountEmail="jkjnbllnl"
+accountPassword="789456"
+accountCity="jaipur"
+console.log(accountId);
+/*
+Prefer not to use var and only use let because of issue 
+in block scope an functional scope
+
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) // we are using node js . not browser
+
+console.log(3+3);
+console.log("Narayan");
+
+let name="Narayan"
+let age=24
+let isLoggedIn= true
+let state= null;
+
+//number -> 2^53
+//bigint ->
+//string ''  ""
+//boolean true false
+//null -> standalone value
+//undefined -> when value is not defined
+//symbol -> unique 
+
+
+//Object
+
+
+console.log(typeof state);
