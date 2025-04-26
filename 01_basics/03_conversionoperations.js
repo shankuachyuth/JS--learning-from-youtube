c7981cb4ff410385a53f034998ff7410fe411d54
SHANKU ACHYUTH <71203243+shankuachyuth@users.noreply.github.com>
2025-04-26 16:40:27 +0000

    conversion

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,15 @@
+let score ="abc"
+
+console.log(typeof score);
+
+let valueInnumber=Number(score)
+console.log(typeof valueInnumber, valueInnumber);
+
+
+//"33"-> 33
+//33abc -> NaN
+//true -> 1 false->0
+let isLoggenIn =1
+let booleanIsLoggedIn=Boolean(isLoggenIn)
+
+console.log(booleanIsLoggedIn);
